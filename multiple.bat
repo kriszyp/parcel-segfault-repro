d4660a15aaf3f7c17704a1b7c7b0ea9cb00bb468
Kris Zyp <kriszyp@gmail.com>
2022-06-19 06:48:17 -0600

    Test runner

---
+++ multiple.bat
@@ -0,0 +1,10 @@
+start /B npx parcel build --no-cache --no-content-hash src/index.html
+start /B npx parcel build --no-cache --no-content-hash src/index.html
+start /B npx parcel build --no-cache --no-content-hash src/index.html
+start /B npx parcel build --no-cache --no-content-hash src/index.html
+start /B npx parcel build --no-cache --no-content-hash src/index.html
+start /B npx parcel build --no-cache --no-content-hash src/index.html
+start /B npx parcel build --no-cache --no-content-hash src/index.html
+start /B npx parcel build --no-cache --no-content-hash src/index.html
+start /B npx parcel build --no-cache --no-content-hash src/index.html
+npx parcel build --no-cache --no-content-hash src/index.html
---
+++ multiple.sh
@@ -0,0 +1,10 @@
+!# /bin/bash
+rm -rf .parcel-cache
+for (( i=1; i<3; i++))
+do
+	rm -rf dist
+	npx parcel build --no-cache --no-content-hash src/index.html &
+	npx parcel build --no-cache --no-content-hash src/index.html &
+	npx parcel build --no-cache --no-content-hash src/index.html &
+	npx parcel build --no-cache --no-content-hash src/index.html || exit 1
+done
